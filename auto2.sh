49c70f2dcf1e9c01adc1c06caf9e0257aabccd2c
AldiGalang <121712363+AldiGalang@users.noreply.github.com>
2025-03-02 05:34:21 +0700

    Update auto2.sh

--- auto2.sh
+++ auto2.sh
@@ -9,7 +9,7 @@ curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/
 # Load new environment variables
 source $HOME/.bashrc
 # Initialize Gaia node
-gaianet init --base $HOME/gaia-node-101 --config https://huggingface.co/gaianet/Qwen2.5-0.5B-Instruct-GGUF/resolve/main/Qwen2.5-0.5B-Instruct-Q5_K_M.gguf
+gaianet init --base $HOME/gaia-node-102 --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
 gaianet config --base $HOME/gaia-node-102 --port 8102
 gaianet init --base $HOME/gaia-node-102
 # Kill any process using port 8102
